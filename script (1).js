551669b64666a4b3f6ec7a7786b8a985044ebf0a
STOLENBOATS <almeida.ricardojorge@gmail.com>
2025-07-28 22:47:51 +0100

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,13 @@
+
+function validarLogin() {
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  if (username === "admin" && password === "Admin2025") {
+    window.location.href = "validador.html";
+    return false;
+  } else {
+    alert("Credenciais inválidas. Tente novamente.");
+    return false;
+  }
+}
